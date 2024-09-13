d184f2aa52e9fbe10d96d99b094e4f05bef17885
Tomo <tomobratic23@gmail.com>
2024-09-13 14:16:17 +0200

    create send message input and button

--- src/features/Chat/MainChat.tsx
+++ src/features/Chat/MainChat.tsx
@@ -1,37 +1,137 @@
 import Profile from "../../assets/profile.png";
-
+import SendRoundedIcon from "@mui/icons-material/SendRounded";
 function MainChat() {
   return (
-    <div className="bg-buttonPurple rounded-2xl grid grid-cols-2 place-items-start">
-      <div className="flex gap-5 items-center text-white p-10">
-        <div>
-          <img src={Profile} alt="Profile Image" className="h-12 rounded-3xl" />
-          <p className="text-white text-center">9:00</p>
+    <section className="bg-buttonPurple rounded-2xl">
+      <div className="grid grid-cols-2 place-items-start mt-8 max-h-[720px] overflow-auto">
+        <div className="flex gap-5 items-center text-white p-10">
+          <div>
+            <img
+              src={Profile}
+              alt="Profile Image"
+              className="h-12 rounded-3xl"
+            />
+            <p className="text-white text-center">9:00</p>
+          </div>
+          <div className="bg-[#2b2738] p-5 rounded-lg">
+            <p>Hi there, How are you</p>
+          </div>
         </div>
-        <div className="bg-[#2b2738] p-5 rounded-lg">
-          <p>Hi there, How are you</p>
+        <div className="flex gap-5 items-center text-white p-10 pt-32 pl-24">
+          <div className="bg-[#2b2738] p-5 rounded-lg">
+            <p>Hi there, How are you</p>
+          </div>
+        </div>
+        <div className="flex gap-5 items-center text-white p-10">
+          <div>
+            <img
+              src={Profile}
+              alt="Profile Image"
+              className="h-12 rounded-3xl"
+            />
+            <p className="text-white text-center">9:00</p>
+          </div>
+          <div className="bg-[#2b2738] p-5 rounded-lg">
+            <p>Hi there, How are you</p>
+          </div>
+        </div>
+        <div className="flex gap-5 items-center text-white p-10 pt-32 pl-24">
+          <div className="bg-[#2b2738] p-5 rounded-lg">
+            <p>Hi there, How are you</p>
+          </div>
+        </div>
+        <div className="flex gap-5 items-center text-white p-10">
+          <div>
+            <img
+              src={Profile}
+              alt="Profile Image"
+              className="h-12 rounded-3xl"
+            />
+            <p className="text-white text-center">9:00</p>
+          </div>
+          <div className="bg-[#2b2738] p-5 rounded-lg">
+            <p>Hi there, How are you</p>
+          </div>
+        </div>
+        <div className="flex gap-5 items-center text-white p-10 pt-32 pl-24">
+          <div className="bg-[#2b2738] p-5 rounded-lg">
+            <p>Hi there, How are you</p>
+          </div>
+        </div>
+        <div className="flex gap-5 items-center text-white p-10">
+          <div>
+            <img
+              src={Profile}
+              alt="Profile Image"
+              className="h-12 rounded-3xl"
+            />
+            <p className="text-white text-center">9:00</p>
+          </div>
+          <div className="bg-[#2b2738] p-5 rounded-lg">
+            <p>Hi there, How are you</p>
+          </div>
+        </div>
+        <div className="flex gap-5 items-center text-white p-10 pt-32 pl-24">
+          <div className="bg-[#2b2738] p-5 rounded-lg">
+            <p>Hi there, How are you</p>
+          </div>
+        </div>
+        <div className="flex gap-5 items-center text-white p-10">
+          <div>
+            <img
+              src={Profile}
+              alt="Profile Image"
+              className="h-12 rounded-3xl"
+            />
+            <p className="text-white text-center">9:00</p>
+          </div>
+          <div className="bg-[#2b2738] p-5 rounded-lg">
+            <p>Hi there, How are you</p>
+          </div>
+        </div>
+        <div className="flex gap-5 items-center text-white p-10 pt-32 pl-24">
+          <div className="bg-[#2b2738] p-5 rounded-lg">
+            <p>Hi there, How are you</p>
+          </div>
+        </div>
+        <div className="flex gap-5 items-center text-white p-10">
+          <div>
+            <img
+              src={Profile}
+              alt="Profile Image"
+              className="h-12 rounded-3xl"
+            />
+            <p className="text-white text-center">9:00</p>
+          </div>
+          <div className="bg-[#2b2738] p-5 rounded-lg">
+            <p>Hi there, How are you</p>
+          </div>
+        </div>
+        <div className="flex gap-5 items-center text-white p-10 pt-32 pl-24">
+          <div className="bg-[#2b2738] p-5 rounded-lg">
+            <p>Hi there, How are you</p>
+          </div>
         </div>
       </div>
-      <div className="flex gap-5 items-center text-white p-10 pt-32">
-        <div className="bg-[#2b2738] p-5 rounded-lg">
-          <p>Hi there, How are you</p>
-        </div>
+      <div className="flex gap-5 px-10 pb-5">
+        <input
+          type="text"
+          placeholder="Type a Message"
+          className="w-full rounded-2xl pl-3"
+        />
+        <button>
+          <SendRoundedIcon
+            sx={{
+              color: "#fff",
+              fontSize: "3.5rem",
+              background: "#2b2738",
+              padding: "10px",
+              borderRadius: "25px",
+            }}
+          />
+        </button>
       </div>
-      <div className="flex gap-5 items-center text-white p-10">
-        <div>
-          <img src={Profile} alt="Profile Image" className="h-12 rounded-3xl" />
-          <p className="text-white text-center">9:00</p>
-        </div>
-        <div className="bg-[#2b2738] p-5 rounded-lg">
-          <p>Hi there, How are you</p>
-        </div>
-      </div>
-      <div className="flex gap-5 items-center text-white p-10 pt-32">
-        <div className="bg-[#2b2738] p-5 rounded-lg">
-          <p>Hi there, How are you</p>
-        </div>
-      </div>
-    </div>
+    </section>
   );
 }
 
