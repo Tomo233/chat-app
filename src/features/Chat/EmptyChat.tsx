98f353a7119470fadafd9d978153c6b1bd92e563
Tomo <tomobratic23@gmail.com>
2024-09-18 14:52:23 +0200

    create empty chat component

---
+++ src/features/Chat/EmptyChat.tsx
@@ -0,0 +1,11 @@
+function EmptyChat() {
+  return (
+    <section className="bg-primaryPurple rounded-2xl w-2/3 flex justify-center items-center">
+      <h2 className="text-white font-semibold text-3xl text-center uppercase">
+        This chat is empty
+      </h2>
+    </section>
+  );
+}
+
+export default EmptyChat;
