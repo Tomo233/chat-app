8fce84524bec2847db7a51aa9b8ecabf29c04545
Tomo <tomobratic23@gmail.com>
2024-12-19 17:42:30 +0100

    navigate to chat page if authenticated user tries to go on login or sign up page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,12 +1,27 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import phone from "../assets/phone.png";
 import Container from "../components/Container";
+import Loader from "../components/Loader";
 import LoginForm from "../features/authentication/LoginForm";
 import LoginPageHeader from "../features/authentication/LoginPageHeader";
 import SignUpForm from "../features/authentication/SignupForm";
+import { useUser } from "../features/authentication/useUser";
 
 function LoginPage() {
   const { pathname } = useLocation();
+  const { user, isLoading } = useUser();
+  const navigate = useNavigate();
+
+  if (isLoading)
+    return (
+      <div className="bg-backgroundColor h-screen flex justify-center items-center">
+        <p className="text-white text-5xl">Loading...</p>
+        <Loader />
+      </div>
+    );
+
+  if (user) navigate("/");
+
   return (
     <div className="bg-backgroundColor h-screen">
       <Container>
