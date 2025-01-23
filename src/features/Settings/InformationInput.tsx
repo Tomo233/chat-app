32d0784e9a6418ec93e8050eb47d7c223bfccfc6
Tomo <tomobratic23@gmail.com>
2025-01-23 14:01:12 +0100

    create reusable input and use it in settings page

---
+++ src/features/Settings/InformationInput.tsx
@@ -0,0 +1,21 @@
+import { forwardRef } from "react";
+
+type InformationInputProps = {
+  value: string;
+  isEditing: boolean;
+};
+
+const InformationInput = forwardRef<HTMLInputElement, InformationInputProps>(
+  ({ value, isEditing }, ref) => {
+    return (
+      <input
+        className="font-medium p-3 bg-secondaryPurple outline-none"
+        value={value}
+        ref={ref}
+        disabled={!isEditing}
+      />
+    );
+  }
+);
+
+export default InformationInput;
--- src/features/Settings/PersonalInformation.tsx
+++ src/features/Settings/PersonalInformation.tsx
@@ -1,60 +1,45 @@
 import EditIcon from "@mui/icons-material/Edit";
 import { UserInfo } from "../../services/authentication";
+import InformationInput from "./InformationInput";
+import { useState } from "react";
 
 function PersonalInformation({ user }: { user: UserInfo }) {
+  const [isEditing, setIsEditing] = useState(false);
   return (
     <section className="border border-secondaryPurple rounded-lg p-3 pb-10 pl-7">
       <div className="flex justify-between items-center">
         <h2 className="font-bold text-3xl py-7">Personal Information</h2>
-        <button className="border border-secondaryPurple px-9 py-3 flex gap-2 rounded-3xl">
+        <button
+          className="border border-secondaryPurple px-9 py-3 flex gap-2 rounded-3xl"
+          onClick={() => setIsEditing((prev) => !prev)}
+        >
           <EditIcon />
-          Edit
+          {!isEditing ? "Edit" : "Save"}
         </button>
       </div>
       <div className="grid grid-cols-2 gap-y-6">
         <div>
           <p>First Name</p>
-          <input
-            className="font-medium p-3 bg-secondaryPurple"
-            value={user?.firstName}
-            disabled
-          />
+          <InformationInput value={user.firstName} isEditing={isEditing} />
         </div>
         <div>
           <p>Last Name</p>
-          <input
-            className="font-medium p-3 bg-secondaryPurple"
-            value={user?.lastName}
-            disabled
-          />
+          <InformationInput value={user.lastName} isEditing={isEditing} />
         </div>
 
         <div>
           <p>Email</p>
-          <input
-            className="font-medium p-3 bg-secondaryPurple"
-            type="text"
-            value={user?.email}
-            disabled
-          />
+          <InformationInput value={user.email} isEditing={isEditing} />
         </div>
         <div>
           <div className="flex gap-5">
             <div>
               <p className="font-medium">Current Password</p>
-              <input
-                className="font-medium p-3 bg-secondaryPurple"
-                value={"**********"}
-                disabled
-              />
+              <InformationInput value={"*********"} isEditing={isEditing} />
             </div>
             <div>
               <p className="font-medium">New Password</p>
-              <input
-                className="font-medium p-3 bg-secondaryPurple"
-                value={"**********"}
-                disabled
-              />
+              <InformationInput value={"*********"} isEditing={isEditing} />
             </div>
           </div>
         </div>
