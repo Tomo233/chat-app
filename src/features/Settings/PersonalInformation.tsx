f5551ff682460f1541af84d8e8b4a8aee5390b83
Tomo <tomobratic23@gmail.com>
2025-04-02 21:59:25 +0200

    redesign PersonalInformation component with improved UI and cleaner structure (no inputs & useless useEffects)

--- src/features/Settings/PersonalInformation.tsx
+++ src/features/Settings/PersonalInformation.tsx
@@ -1,55 +1,29 @@
-import EditIcon from "@mui/icons-material/Edit";
-import { UserInfo } from "../../services/authentication";
-import InformationInput from "./InformationInput";
-import { useEffect, useState } from "react";
-import FileInput from "../../components/FileInput";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { UserInfo } from "../../services/authentication";
 import { SignupAndProfileInputs } from "../authentication/SignupForm";
 import { useEditUser } from "../authentication/useEditUser";
 import Loader from "../../components/Loader";
 import ConfirmPasswordPopup from "../authentication/ConfirmPasswordPopup";
+import EditIcon from "@mui/icons-material/Edit";
+
 function PersonalInformation({ user }: { user: UserInfo }) {
-  const { isEditingUser, editUser } = useEditUser(); // Move this to top-level so all children share the same mutation state
+  const { email, firstName, lastName, location } = user;
+  const { isEditingUser, editUser } = useEditUser();
   const [editingStatus, setEditingStatus] = useState<
     "not-editing" | "editing" | "saving"
   >("not-editing");
 
-  const { register, handleSubmit, setValue, watch, reset, control } =
-    useForm<SignupAndProfileInputs>({
-      defaultValues: {
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        password: "",
-        confirmOrNewPassword: "",
-        avatar: null,
-      },
-    });
-
-  useEffect(() => {
-    if (
-      user.email !== watch("email") ||
-      user.firstName !== watch("firstName") ||
-      user.lastName !== watch("lastName") ||
-      watch("avatar") !== null ||
-      watch("confirmOrNewPassword") !== ""
-    ) {
-      setEditingStatus("editing");
-    } else {
-      setEditingStatus("not-editing");
-    }
-  }, [
-    user,
-    watch("email"),
-    watch("firstName"),
-    watch("lastName"),
-    watch("avatar"),
-    watch("confirmOrNewPassword"),
-  ]);
-
-  useEffect(() => {
-    if (editingStatus === "not-editing") reset();
-  }, [editingStatus]);
+  const { handleSubmit, control } = useForm<SignupAndProfileInputs>({
+    defaultValues: {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      password: "",
+      confirmOrNewPassword: "",
+      avatar: null,
+    },
+  });
 
   if (isEditingUser) return <Loader />;
 
@@ -57,48 +31,41 @@ function PersonalInformation({ user }: { user: UserInfo }) {
     <section className="border border-secondaryPurple rounded-lg p-3 pb-10 pl-7">
       <div className="flex justify-between items-center">
         <h2 className="font-bold text-3xl py-7">Personal Information</h2>
-        {editingStatus !== "not-editing" && (
-          <button
-            className="border border-secondaryPurple px-9 py-3 flex gap-2 rounded-3xl"
-            onClick={() => setEditingStatus("saving")}
-          >
-            <span>
-              <EditIcon />
-              Save
-            </span>
-          </button>
-        )}
+        <button
+          className="border border-secondaryPurple px-12 py-3 flex gap-2 rounded-3xl"
+          onClick={() => setEditingStatus("saving")}
+        >
+          <span>
+            <EditIcon />
+            Edit
+          </span>
+        </button>
       </div>
-      <form className="grid grid-cols-3 gap-y-6 items-end">
+      <form className="grid grid-cols-2 gap-y-6 items-end">
         <div>
-          <p>First Name</p>
-          <InformationInput {...register("firstName")} />
+          <p className="text-lg text-[#d6d4d4]">First Name</p>
+          <p className="text-lg font-semibold">{firstName}</p>
+        </div>
+        <div>
+          <p className="text-lg text-[#d6d4d4]">Email</p>
+          <p className="text-lg font-semibold">{email}</p>
+        </div>
+        <div>
+          <p className="text-lg text-[#d6d4d4]">Last Name</p>
+          <p className="text-lg font-semibold">{lastName}</p>
         </div>
 
         <div>
-          <p>Last Name</p>
-          <InformationInput {...register("lastName")} />
-        </div>
-
-        <div>
-          <FileInput
-            {...register("avatar")}
-            setValue={setValue}
-            editingStatus={editingStatus}
-          />
-        </div>
-        <div>
-          <p>Email</p>
-          <InformationInput {...register("email")} />
-        </div>
-
-        <div>
-          <p className="font-medium">New Password</p>
-          <InformationInput
-            {...register("confirmOrNewPassword")}
-            placeholder="*********"
-            type="password"
-          />
+          {location ? (
+            <>
+              <p className="text-lg text-[#d6d4d4]">Location</p>
+              <p className="text-lg font-semibold">{location}</p>
+            </>
+          ) : (
+            <p className="text-lg text-[#e0dede] mb-3 font-medium">
+              Location not provided !
+            </p>
+          )}
         </div>
       </form>
       {editingStatus === "saving" && (
